02664d6a667fe9c8f24a95544e346067569572ed
Victor Lyamkin <just-do-it5@yandex.ru>
2018-03-28 18:59:07 +0700

    corrections

--- lesson5/gallery/login.php
+++ lesson5/gallery/login.php
@@ -13,27 +13,26 @@ if (isset($_POST['login']) && isset($_POST['password'])) {
         exit;
     }
 }
+?>
 
-if (null == getCurrentUser()) { ?>
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Login</title>
-    </head>
-    <body>
-    <form action="/lesson5/gallery/login.php" method="post">
-        <label for="login-field">Ваш логин</label>
-        <input type="text" name="login" id="login-field">
-        <br><br>
-        <label for="password-field">Ваш пароль</label>
-        <input type="password" name="password" id="password-field">
-        <br><br>
-        <input type="submit" value="Войти">
-    </form>
-    </body>
-    </html>
-<?php } ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+<form action="/lesson5/gallery/login.php" method="post">
+    <label for="login-field">Ваш логин</label>
+    <input type="text" name="login" id="login-field">
+    <br><br>
+    <label for="password-field">Ваш пароль</label>
+    <input type="password" name="password" id="password-field">
+    <br><br>
+    <input type="submit" value="Войти">
+</form>
+</body>
+</html>
